8344674ed3893aae45523383f47c84aea0597b29
Neo <neocamanga@knights.ucf.edu>
2022-12-08 23:17:46 -0500

    [solved] Day 8 part 1. Refactored

--- 8/a.ts
+++ 8/a.ts
@@ -1,13 +1,12 @@
-const input: string = await Deno.readTextFile("./exampleInput.txt");
-// const input: string = await Deno.readTextFile("./input.txt");
+// const input: string = await Deno.readTextFile("./exampleInput.txt");
+const input: string = await Deno.readTextFile("./input.txt");
 
 const splitInput = input
 	.trimEnd()
 	.split("\r\n")
 	.map(i => i.split("").map(Number))
 
-console.log(splitInput);
-console.log(splitInput[2][3]);
+// console.log(splitInput);
 
 // count outer trees
 // 4 * (n - 2) + 4 === 4n - 4 === 4 * (n - 1)
@@ -21,66 +20,81 @@ console.log(splitInput[2][3]);
 // check left
 // check right
 
-function checkRight(input: number[][], startCoordinates: [number, number]) {
+// function checkRight(input: number[][], startCoordinates: [number, number]) {
+// 	let [i, j] = startCoordinates;
+// 	const treeHeight = input[i][j];
+
+// 	while (++j < input.length)
+// 		if (treeHeight <= input[i][j]) return false;
+
+// 	return true;
+// }
+
+
+// function checkLeft(input: number[][], startCoordinates: [number, number]) {
+// 	let [i, j] = startCoordinates;
+// 	const treeHeight = input[i][j];
+
+// 	while (--j >= 0)
+// 		if (treeHeight <= input[i][j]) return false;
+
+// 	return true;
+// }
+
+// function checkDown(input: number[][], startCoordinates: [number, number]) {
+// 	let [i, j] = startCoordinates;
+// 	const treeHeight = input[i][j];
+
+// 	while (++i < input.length)
+// 		if (treeHeight <= input[i][j]) return false;
+
+// 	return true;
+// }
+
+// function checkUp(input: number[][], startCoordinates: [number, number]) {
+// 	let [i, j] = startCoordinates;
+// 	const treeHeight = input[i][j];
+
+// 	while (--i >= 0)
+// 		if (treeHeight <= input[i][j]) return false;
+
+// 	return true;
+// }
+
+type Direction = "up" | "down" | "left" | "right";
+
+function checkDirections(input: number[][], startCoordinates: [number, number], direction: Direction) {
 	let [i, j] = startCoordinates;
 	const treeHeight = input[i][j];
+	let loopCondition: () => boolean;
 
-	while (++j < input.length)
-		if (treeHeight <= input[i][j]) return false;
+	switch (direction) {
+		case "up": loopCondition = () => --i >= 0; break;
+		case "down": loopCondition = () => ++i < input.length; break;
+		case "left": loopCondition = () => --j >= 0; break;
+		case "right":
+		default: loopCondition = () => ++j < input.length;
+	}
 
-	return true;
-}
-
-
-function checkLeft(input: number[][], startCoordinates: [number, number]) {
-	let [i, j] = startCoordinates;
-	const treeHeight = input[i][j];
-
-	while (--j >= 0)
-		if (treeHeight <= input[i][j]) return false;
-
-	return true;
-}
-
-function checkDown(input: number[][], startCoordinates: [number, number]) {
-	let [i, j] = startCoordinates;
-	const treeHeight = input[i][j];
-
-	while (++i < input.length)
-		if (treeHeight <= input[i][j]) return false;
-
-	return true;
-}
-
-function checkUp(input: number[][], startCoordinates: [number, number]) {
-	let [i, j] = startCoordinates;
-	const treeHeight = input[i][j];
-
-	while (--i >= 0)
+	while (loopCondition())
 		if (treeHeight <= input[i][j]) return false;
 
 	return true;
 }
 
 function isTreeVisible(input: number[][], startCoordinates: [number, number]) {
-	if (checkUp(input, startCoordinates)) return true;
-	if (checkDown(input, startCoordinates)) return true;
-	if (checkLeft(input, startCoordinates)) return true;
-	if (checkRight(input, startCoordinates)) return true;
-	return false;
+	const directions: Direction[] = ["up", "down", "left", "right"];
+
+	return directions.some(direction => checkDirections(input, startCoordinates, direction));
 }
 
 function countInnerTrees(input: number[][]) {
 	let visibleCount = 0;
 
-	for (let i = 1; i < input.length - 1; i++) {
-		for (let j = 1; j < input.length - 1; j++) {
-			if (isTreeVisible(input, [i, j])) {
+	for (let i = 1; i < input.length - 1; i++)
+		for (let j = 1; j < input.length - 1; j++)
+			if (isTreeVisible(input, [i, j]))
 				visibleCount++;
-				continue;
-			}
-		}
-	}
 
 	return visibleCount;
 }
